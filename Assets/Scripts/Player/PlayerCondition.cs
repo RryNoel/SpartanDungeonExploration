91897b0df01c4777a69631fb6b1042cb6fd5d5c0
JeongSeok <97824309+RryNoel@users.noreply.github.com>
2024-05-28 16:49:43 +0900

    Feature : Player 기본 상태 구현

---
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class PlayerCondition : MonoBehaviour
+{
+    public UICondition uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+    Condition hunger { get { return uiCondition.hunger; } }
+    Condition stamina { get { return uiCondition.stamina; } }
+
+    public float noHungerHealthDecay;
+
+    private void Update()
+    {
+        hunger.Subtract(hunger.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if(hunger.curValue == 0f)
+        {
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+
+        if(health.curValue == 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Eat(float amount)
+    {
+        hunger.Add(amount);
+    }
+
+    private void Die()
+    {
+        Debug.Log("죽었다!");
+    }
+}
--- Assets/Scripts/UI/UICondition.cs
+++ Assets/Scripts/UI/UICondition.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class UICondition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Condition health;
+    public Condition hunger;
+    public Condition stamina;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        CharacterManager.Instance.Player.condition.uiCondition = this;
     }
 }
