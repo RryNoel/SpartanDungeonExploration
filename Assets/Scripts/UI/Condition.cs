5b0cf1c7b383ab52eb68f3b6f748a165f1019a90
JeongSeok <97824309+RryNoel@users.noreply.github.com>
2024-05-28 16:24:17 +0900

    Add : Condition 스크립트 세팅

--- Assets/Scripts/UI/Condition.cs
+++ Assets/Scripts/UI/Condition.cs
@@ -1,8 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Condition : MonoBehaviour
 {
-    
+    public float curValue;
+    public float startValue;
+    public float maxValue;
+    public float passiveValue;
+    public Image uiBar;
+
+    private void Start()
+    {
+        curValue = startValue;
+    }
+
+    private void Update()
+    {
+        uiBar.fillAmount = GetPercentage();
+    }
+
+    float GetPercentage()
+    {
+        return curValue / maxValue;
+    }
+
+    public void Add(float value)
+    {
+        curValue = Mathf.Min(curValue + value, maxValue);
+    }
+
+    public void Subtract(float value)
+    {
+        curValue = Mathf.Max(curValue - value, 0);
+    }
 }
