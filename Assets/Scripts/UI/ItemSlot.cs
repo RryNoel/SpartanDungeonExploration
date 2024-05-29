a682dfec25261886cbe81f9c6bf051e587738eee
JeongSeok <97824309+RryNoel@users.noreply.github.com>
2024-05-29 10:39:12 +0900

    Add : 인벤토리 UI 및 스크립트 추가

---
+++ Assets/Scripts/UI/ItemSlot.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSlot : MonoBehaviour
+{
+    public ItemData item;
+
+    public UIInventory inventory;
+
+    public int index;
+    public bool equipped;
+    public int quantity;
+}
---
+++ Assets/Scripts/UI/UIInventory.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIInventory : MonoBehaviour
+{
+    public ItemSlot[] slots;
+
+    public GameObject inventoryWindow;
+    public Transform slotPanel;
+
+    [Header("Select Item")]
+    public TextMeshProUGUI selectedItemName;
+    public TextMeshProUGUI selectedItemDescription;
+    public TextMeshProUGUI selectedStatName;
+    public TextMeshProUGUI selectedStatValue;
+    public GameObject useButton;
+    public GameObject equipButton;
+    public GameObject unEquipButton;
+    public GameObject dropButton;
+
+    private PlayerController controller;
+    private PlayerCondition condition;
+
+    private void Start()
+    {
+        controller = CharacterManager.Instance.Player.controller;
+        condition = CharacterManager.Instance.Player.condition;
+
+        inventoryWindow.SetActive(false);
+        slots = new ItemSlot[slotPanel.childCount];
+
+        for(int i = 0; i < slots.Length; i++)
+        {
+            slots[i] = slotPanel.GetChild(i).GetComponent<ItemSlot>();
+            slots[i].index = i;
+            slots[i].inventory = this;
+        }
+
+        ClearSelectedItemWindow();
+    }
+
+    void ClearSelectedItemWindow()
+    {
+        selectedItemName.text = string.Empty;
+        selectedItemDescription.text = string.Empty;
+        selectedStatName.text = string.Empty;
+        selectedStatValue.text = string.Empty;
+
+        useButton.SetActive(false);
+        equipButton.SetActive(false);
+        unEquipButton.SetActive(false);
+        dropButton.SetActive(false);
+    }
+}
